28e2015edb00999cb6bc61d84158d056acd1bad3
marioquiroga <mquiroga@dosalcubo.com>
2021-08-20 23:44:13 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,60 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg')
+})
+
+renderer.setPixelRatio(window.devicePixelRatio)
+renderer.setSize(window.innerWidth, window.innerHeight)
+camera.position.setZ(30)
+camera.position.setX(-3)
+
+//  Torus
+const donutTexture = new THREE.TextureLoader().load('donut.jpg')
+const geometry = new THREE.TorusGeometry(10, 4, 16, 100)
+const material = new THREE.MeshStandardMaterial({map:donutTexture})
+const torus = new THREE.Mesh(geometry, material)
+scene.add(torus)
+
+//  Light
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(30, 35, 35)
+const ambientLight = new THREE.AmbientLight(0xffffff)
+const gridHelper = new THREE.GridHelper(200, 50)
+//const lightHelper = new THREE.PointLightHelper(pointLight)
+scene.add(pointLight, ambientLight, gridHelper/* , lightHelper */)
+
+// Controls
+const controls = new OrbitControls(camera, renderer.domElement)
+
+//  Stars
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.15, 24, 24)
+  const material = new THREE.MeshStandardMaterial({color:0xffffff})
+  const star = new THREE.Mesh(geometry, material)
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100))
+  star.position.set(x, y, z)
+
+  scene.add(star)
+}
+Array(200).fill().forEach(addStar)
+
+// Textures
+const spaceTexture = new THREE.TextureLoader().load('space.jpg')
+scene.background = spaceTexture
+
+function animate() {
+  requestAnimationFrame(animate)
+  torus.rotation.x += 0.01
+  torus.rotation.y += 0.001
+  torus.rotation.z += 0.01
+
+  controls.update()
+  renderer.render(scene, camera)
+}
+animate()
